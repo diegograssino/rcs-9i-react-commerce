ce33cd950f34cb41d9a9fd09eaa29079c116739d
Diego Grassino <diegograssino@MacBook-Pro-de-Diego.local>
2022-09-14 21:22:01 -0300

    Fin de clase 2

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+
+// Esto es un comentario
+function App() {
+  return (
+    <div>
+      <h1>Hola Roling con React!</h1>
+      <button>El botón de tu vecino</button>
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <h2>Soy el header</h2>;
+};
+
+export default Header;
